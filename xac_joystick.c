d9e9d475c10cbf0baa74e1f51de64cf8250dc23e
taka-tuos <hvcnes@gmail.com>
2025-03-13 15:27:38 +0900

    PSコン仮対応

--- xac_joystick.c
+++ xac_joystick.c
@@ -37,6 +37,7 @@
 #include "usb_descriptors.h"
 
 #include "hardware/gpio.h"
+#include "pico/multicore.h"
 
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
@@ -57,6 +58,7 @@ static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
 
 void led_blinking_task(void);
 void hid_task(void);
+void core1_task(void);
 
 #pragma region 定数等
 
@@ -113,6 +115,13 @@ const int map2[11] = {
 	0,1,2,3,4,5,6,
 	0,1,2,3
 };
+
+// 我らの聖典ps_jpn.txtを崇めよ！
+const int psx_att = -1; // CS
+const int psx_sck = -1; // SCK
+const int psx_dat = -1; // MISO
+const int psx_cmd = -1; // MOSI
+const int psx_ack = -1; // ACK
 
 #pragma endregion
 
@@ -181,6 +190,9 @@ int main(void) {
 	scr_delta = !gpio_get(keys[3]) ? 4 : scr_delta;
 
 	// 6倍は実装予定なし
+
+	// PS用タスクを起動
+	multicore_launch_core1(core1_task);
 
 	// ぶんまわし
 	while (1) {
@@ -351,6 +363,158 @@ void hid_task(void) {
 	/*------------- Joystick -------------*/
 	if (tud_hid_ready()) {
 		tud_hid_report(0x00, &report, sizeof(report));
+	}
+}
+
+#pragma endregion
+
+#pragma region PSコン処理
+
+// redditの仙人に感謝
+enum transfer_state {
+	TRANSFER_STATE_IDLE,
+	TRANSFER_STATE_AWAITING_COMMAND,
+	TRANSFER_STATE_SENDING_DATA,
+	TRANSFER_STATE_FINISHING,
+} state;
+
+// 0: 立下り
+void psx_waitedge(int edge) {
+	if(edge == 0) {
+		while(gpio_get(psx_sck) == 1);
+	} else if(edge == 1) {
+		while(gpio_get(psx_sck) == 0);
+	}
+}
+
+// 綴りはわざと
+// mode 3, LSB first
+uint8_t psx_transfur(uint8_t send) {
+	uint8_t dat = 0;
+
+	for(int i = 0; i < 8; i++) {
+		// 最初に立下りが来る
+		psx_waitedge(0);
+
+		// オープンドレイン駆動
+		gpio_set_dir(psx_dat, !(send & (1 << i)));
+
+		// 立ち上がり待ち
+		psx_waitedge(0);
+
+		// 読んで入れる
+		dat |= ((gpio_get(psx_cmd) ? 1 : 0) << i);
+	}
+
+	// Hi-Zに戻す
+	gpio_set_dir(psx_dat, false);
+}
+
+// Picoには2コア目があってぇ
+// SPIペリフェラルを使う→割り込みはCore0だけしか受け取れないのでUSBとぶつかる
+// PIO→めんどい
+// 雑ポーリング→かんたん(4MHzぐらいまでなら行けてしまうので)
+void core1_task() {
+	// 入力
+	gpio_set_dir(psx_sck, false);
+	gpio_set_dir(psx_att, false);
+	gpio_set_dir(psx_cmd, false);
+
+	// オープンドレイン
+	gpio_set_dir(psx_dat, false);
+	gpio_put(psx_dat, false);
+
+	gpio_set_dir(psx_ack, false);
+	gpio_put(psx_ack, false);
+
+	// 出力
+	// なんと、ない
+
+	uint8_t data_to_send = 0;
+	uint8_t send = 0xff;
+
+	volatile int state = TRANSFER_STATE_IDLE;
+
+	// ぶん回すぜ！
+	while(1) {
+		// CSがLOWになるまで待つ
+		while(gpio_get(psx_att) == 1) state = TRANSFER_STATE_IDLE;
+
+		int ack = 1;
+		
+		uint8_t data = psx_transfur(send);
+		uint8_t command = 0;
+
+		// コピペ元: http://benryves.com/bin/playstation-arcade-stick.zip
+		switch(state) {
+			case TRANSFER_STATE_IDLE:
+				switch (data) {
+					case 0x01:
+						state = TRANSFER_STATE_AWAITING_COMMAND;
+						send = 0x41; // Digital pad.
+						break;
+					default:
+						state = TRANSFER_STATE_IDLE;
+						ack = 0;
+						break;
+				}
+				break;
+			case TRANSFER_STATE_AWAITING_COMMAND:
+				command = data;
+				switch (command) {
+					case 0x42:
+						state = TRANSFER_STATE_SENDING_DATA;
+						data_to_send = 2; // 2 bytes of data.
+						send = 0x5A; // Data coming.
+						break;
+					default:
+						state = TRANSFER_STATE_IDLE;
+						ack = 0;
+						break;
+				}
+				break;
+			case TRANSFER_STATE_SENDING_DATA:
+				switch (2 - data_to_send) {
+					case 0:
+						// 1バイト目
+						send = 0xff;
+						break;
+					case 1:
+						// 2バイト目
+						send = 0xff;
+						break;
+				}
+				if (--data_to_send == 0) {
+					state = TRANSFER_STATE_FINISHING;
+				}
+				break;
+			case TRANSFER_STATE_FINISHING:
+				state = TRANSFER_STATE_IDLE;
+				ack = 0;
+				break;
+		}
+
+		if(ack && !gpio_get(psx_att)) {
+			// 5us待って
+			uint64_t start = time_us_64();
+			while(time_us_64() - start < 5);
+
+			// LOWにして
+			gpio_set_dir(psx_att, true);
+
+			// 2us待って
+			start = time_us_64();
+			while(time_us_64() - start < 2);
+
+			// HIGHにする
+			gpio_set_dir(psx_att, false);
+
+			// マイクロ秒は32bitだと72分でラップアラウンドするので狂う
+			// 64bitなら30万年
+		} else {
+			state = TRANSFER_STATE_IDLE;
+			while(!gpio_get(psx_att));
+		}
 	}
 }
 
