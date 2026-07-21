67c8d62a6b30a12ed859cf764b9d855e070f51cb
taka-tuos <hvcnes@gmail.com>
2026-07-21 22:22:14 +0900

    最終版

--- devices/dao_fps.h
+++ devices/dao_fps.h
@@ -5,19 +5,19 @@
 
 // DAO FPS 用基板
 const int keys[KEY_COUNT] = {
-	BTN_DISABLED, BTN_DISABLED, BTN_DISABLED, BTN_DISABLED, BTN_DISABLED, BTN_DISABLED, BTN_DISABLED,
-	BTN_DISABLED, BTN_DISABLED, BTN_DISABLED, BTN_DISABLED
+	4, 8, 5, 9, 6, 10, 7,
+	2, 13, BTN_DISABLED, BTN_DISABLED
 };
 
 const int scr[SCR_COUNT] = {
-	BTN_DISABLED, BTN_DISABLED
+	11, 12
 };
 
-const bool psx_enable = false;
-const int psx_att = -1; // CS
-const int psx_sck = -1; // SCK
-const int psx_cmd = -1; // MOSI
-const int psx_dat = -1; // MISO
-const int psx_ack = -1; // ACK
+const bool psx_enable = true;
+const int psx_att = 27; // CS
+const int psx_sck = 26; // SCK
+const int psx_cmd = 28; // MOSI
+const int psx_dat = 29; // MISO
+const int psx_ack = 15; // ACK
 
 #endif // _DEVICES_DAO_FPS_H_
